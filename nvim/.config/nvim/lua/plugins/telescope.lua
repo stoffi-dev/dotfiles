264b1108d356ef9383d936d4cc787fc6bf240cea
chris <cg@xps-13>
2022-05-05 11:29:40 +1000

    Added new default width to telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ require('telescope').setup({
   defaults = {
     layout_strategy = 'vertical',
     layout_config = {
-      vertical = { width = 0.5 }
+      vertical = { width = 0.75 }
       -- other layout configuration here
     },
     -- other defaults configuration here
