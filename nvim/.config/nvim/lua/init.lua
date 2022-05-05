de1653642cff2828a9ec592b01d29104867c6fee
chris <cg@xps-13>
2022-05-05 11:22:49 +1000

    Updated nvim telecope previewer

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,3 +1,4 @@
 require('plugins')
 require('keymappings')
 require('./lsp/python-ls')
+require('./plugins/telescope')
---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+require('telescope').setup({
+  defaults = {
+    layout_strategy = 'vertical',
+    layout_config = {
+      vertical = { width = 0.5 }
+      -- other layout configuration here
+    },
+    -- other defaults configuration here
+  },
+  -- other configuration values here
+})
