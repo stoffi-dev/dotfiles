eada323853e131ab57bde2224317f447d1f9ba1f
chris <cg@xps-13>
2023-11-22 16:37:50 +1000

    Telescope configuration rework

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -10,8 +10,23 @@
 --  -- other configuration values here })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', 'C-p', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+telescope.setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-j>"] = actions.move_selection_next,
+            }
+        }
+    }
+})
+
+--vim.keymap.set('i', 'C-k', actions.move_selection_previous)
+--vim.keymap.set('i', 'C-j', actions.move_selection_next)
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Fuzzy find files in cwd"})
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, {desc = "Find string in cwd"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Show buffers"})
